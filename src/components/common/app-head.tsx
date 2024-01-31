a768bf6ce79757036de09de2179ef09480c36dcb
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-02-01 04:25:39 +1200

    Update app-head.tsx

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -7,8 +7,6 @@ export function AppHead(): JSX.Element {
       <meta name='og:title' content='Aria+' />
       <link rel='icon' href='/favicon.ico' />
       <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
-      <meta name='twitter:site' content='@ccrsxx' />
-      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   );
 }
