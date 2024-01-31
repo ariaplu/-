4176a6df07eae6fb514ce6cb30f0ee98100b9013
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-31 23:30:08 +1200

    Update login-main.tsx

--- src/components/login/login-main.tsx
+++ src/components/login/login-main.tsx
@@ -23,9 +23,8 @@ export function LoginMain(): JSX.Element {
         </i>
       </div>
       <div className='flex flex-col items-center justify-between gap-6 p-8 lg:items-start lg:justify-center'>
-        <i className=' self-center lg:mb-10 lg:self-auto'>
+        <i className=' self-center'>
         <ArplImage
-          className='arpli'
           imgClassName='arplicon'
           blurClassName='none'
           src='/assets/ariaplus.svg'
