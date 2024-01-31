7596b71628a516f1faa422533f8a97c05dd431ab
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-31 15:06:02 +1200

    Update custom-icon.tsx

--- src/components/ui/custom-icon.tsx
+++ src/components/ui/custom-icon.tsx
@@ -32,7 +32,7 @@ export function CustomIcon({
 
 function AriaPlusIcon({ className }: IconProps): JSX.Element {
   return (
-   <img className="h-6 w-6" src="./images/ariaplus.svg" alt="Aria+" />
+   <Image className="h-6 w-6" src="./images/ariaplus.svg" alt="Aria+" />
   );
 }
 
