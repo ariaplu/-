f920797c5622c5db9a9213dbc3920c1935aeeb9f
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-31 22:27:41 +1200

    Update ariaplus.tsx

--- src/components/ui/ariaplus.tsx
+++ src/components/ui/ariaplus.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
-import ariaplus from '../assets/ariaplus.svg';
+import cn from 'clsx';
+import type { ReactNode } from 'react';
+import type { ImageProps } from 'next/image';
 
-const AriaplusImage: React.FC = () => {
+type NextImageProps = {
+  alt: string;
+  width?: string | number;
+  children?: ReactNode;
+  useSkeleton?: boolean;
+  imgClassName?: string;
+  previewCount?: number;
+  blurClassName?: string;
+} & ImageProps;
+
+/**
+ *
+ * @description Must set width and height, if not add layout='fill'
+ * @param useSkeleton add background with pulse animation, don't use it if image is transparent
+ */
+export function ArplImage({
+  src,
+  alt,
+  width,
+  height,
+  children,
+  className,
+  useSkeleton,
+  imgClassName,
+  previewCount,
+  blurClassName,
+  ...rest
+}: NextImageProps): JSX.Element {
+  const [loading, setLoading] = useState(!!useSkeleton);
+
+  const handleLoad = (): void => setLoading(false);
+
   return (
-    <Image src={ariaplus} />
+    <figure style={{ width }} className={className}>
+      <Image
+        className={cn(
+          imgClassName,
+          loading
+            ? blurClassName ??
+                'animate-pulse bg-light-secondary dark:bg-dark-secondary'
+            : previewCount === 1
+            ? '!h-auto !min-h-0 !w-auto !min-w-0 rounded-lg object-contain'
+            : 'object-cover'
+        )}
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+        onLoadingComplete={handleLoad}
+        layout='responsive'
+        {...rest}
+      />
+      {children}
+    </figure>
   );
-};
-
-export default AriaplusImage;
+}
