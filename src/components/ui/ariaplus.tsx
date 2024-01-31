3fd994ce8701cbb59ac6ce21cfc9cb0eee011cfc
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-02-01 01:36:54 +1200

    Update ariaplus.tsx

--- src/components/ui/ariaplus.tsx
+++ src/components/ui/ariaplus.tsx
@@ -45,7 +45,7 @@ export function ArplImage({
             ? blurClassName ??
                 'animate-pulse bg-light-secondary dark:bg-dark-secondary'
             : previewCount === 1
-            ? '!h-auto !min-h-0 !w-auto !min-w-0 rounded-lg object-contain'
+            ? '!h-24 !w-24 rounded-lg object-contain'
             : 'object-cover'
         )}
         src={src}
