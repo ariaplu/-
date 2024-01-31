65ee92bf5958eff6bd41e1047279fb1093b1edfd
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-31 16:33:02 +1200

    Update custom-icon.tsx

--- src/components/ui/custom-icon.tsx
+++ src/components/ui/custom-icon.tsx
@@ -11,13 +11,14 @@ type CustomIconProps = IconProps & {
 };
 
 const Icons = {
-  AppleIcon
+  PinIcon,
+  AppleIcon,
   PinOffIcon,
   GoogleIcon,
   AriaPlusIcon,
   FeatherIcon,
   SpinnerIcon,
-  TriangleIcon,
+  TriangleIcon
 };
 
 export function CustomIcon({
@@ -31,7 +32,11 @@ export function CustomIcon({
 
 function AriaPlusIcon({ className }: IconProps): JSX.Element {
   return (
-   <svg className="h-6 w-6" src="./images/ariaplus.svg" alt="Aria+" </svg>
+    <svg className={cn('fill-current', className)} viewBox='0 0 24 24'>
+      <g>
+        <path d='M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z' />
+      </g>
+    </svg>
   );
 }
 
@@ -125,6 +130,27 @@ function TriangleIcon({ className }: IconProps): JSX.Element {
   );
 }
 
+function PinIcon({ className }: IconProps): JSX.Element {
+  return (
+    <svg
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M15 4.5l-4 4l-4 1.5l-1.5 1.5l7 7l1.5 -1.5l1.5 -4l4 -4' />
+      <line x1='9' y1='15' x2='4.5' y2='19.5' />
+      <line x1='14.5' y1='4' x2='20' y2='9.5' />
+    </svg>
+  );
+}
 
 function PinOffIcon({ className }: IconProps): JSX.Element {
   return (
