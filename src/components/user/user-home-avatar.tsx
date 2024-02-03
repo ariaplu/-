b87dceaa5f03d3d3c54ff4b6906b4325f6a01550
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-03 15:47:31 +1200

    Update user-home-avatar.tsx

--- src/components/user/user-home-avatar.tsx
+++ src/components/user/user-home-avatar.tsx
@@ -25,7 +25,7 @@ export function UserHomeAvatar({
         />
       </Modal>
       <Button
-        className='accent-tab absolute -mt-3 aspect-square w-24 -translate-y-1/2 overflow-hidden p-0 
+        className='accent-tab absolute -mt-3 aspect-square w-24 -translate-y-1/2 overflow-none p-0 
                    disabled:cursor-auto disabled:opacity-100 xs:w-32 sm:w-36
                    [&:hover>figure>span]:brightness-75'
         onClick={openModal}
@@ -35,7 +35,7 @@ export function UserHomeAvatar({
           <NextImage
             useSkeleton
             className='hover-animation relative h-full w-full bg-main-background
-                       inner:!m-1 inner:rounded-full inner:transition inner:duration-200'
+                       inner:!m-1 inner:rounded-xl inner:transition inner:duration-200'
             imgClassName='rounded-full'
             src={profileData.src}
             alt={profileData.alt}
