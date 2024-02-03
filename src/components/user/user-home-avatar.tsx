f14daf63efab0a0f8cd3e0c0d21fbfb027e75084
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-03 16:01:23 +1200

    Update user-home-avatar.tsx

--- src/components/user/user-home-avatar.tsx
+++ src/components/user/user-home-avatar.tsx
@@ -35,8 +35,8 @@ export function UserHomeAvatar({
           <NextImage
             useSkeleton
             className='hover-animation relative h-full w-full bg-main-background
-                       inner:!m-1 inner:rounded-xl inner:transition inner:duration-200'
-            imgClassName='rounded-full'
+                       inner:!m-1 inner:rounded-full inner:transition inner:duration-200'
+            imgClassName='rounded-xl'
             src={profileData.src}
             alt={profileData.alt}
             layout='fill'
