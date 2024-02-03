07119cec2201332b34e601f84f777bea66a5e7c9
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-03 12:55:14 +1200

    Update trends.tsx

--- src/pages/trends.tsx
+++ src/pages/trends.tsx
@@ -18,8 +18,8 @@ export default function Bookmarks(): JSX.Element {
 
   return (
     <MainContainer>
-      <SEO title='Trends / Twitter' />
-      <MainHeader useActionButton title='Trends' action={back}>
+      <SEO title='Aria+ tv' />
+      <MainHeader useActionButton title='tv' action={back}>
         <Button
           className='dark-bg-tab group relative ml-auto cursor-not-allowed p-2 hover:bg-light-primary/10
                      active:bg-light-primary/20 dark:hover:bg-dark-primary/10 dark:active:bg-dark-primary/20'
