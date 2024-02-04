cfb7bc0c6f3bf0d52e4b79eae96c2f7190d0bf4b
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-04 21:31:43 +1200

    Update tv.tsx

--- src/pages/tv.tsx
+++ src/pages/tv.tsx
@@ -104,9 +104,7 @@ export default function Bookmarks(): JSX.Element {
           />
         ) : (
           <AnimatePresence mode='popLayout'>
-            {tweetData?.map((tweet) => (
-              <Tweet {...tweet} key={tweet.id} />
-            ))}
+
           </AnimatePresence>
         )}
       </section>
