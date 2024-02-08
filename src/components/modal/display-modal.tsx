b5d672f0e75e7b035a41040b0a82bc80ee0200ed
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-08 13:09:43 +1200

    Update display-modal.tsx

--- src/components/modal/display-modal.tsx
+++ src/components/modal/display-modal.tsx
@@ -30,7 +30,7 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
       <div className='flex flex-col gap-3 text-center'>
         <h2 className='text-2xl font-bold'>Customize your view</h2>
         <p className='text-light-secondary dark:text-dark-secondary'>
-          These settings affect all the Twitter accounts on this browser.
+          These settings affect all the Aria+ accounts on this browser.
         </p>
       </div>
       <article
@@ -38,12 +38,12 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
                    border-light-border px-4 py-3 dark:border-dark-border'
       >
         <div className='grid grid-cols-[auto,1fr] gap-3'>
-          <UserAvatar src='/assets/twitter-avatar.jpg' alt='Twitter' />
+          <UserAvatar src='/assets/twitter-avatar.jpg' alt='Aria+' />
           <div>
             <div className='flex gap-1'>
-              <UserName verified name='Twitter' />
+              <UserName verified name='Aria+' />
               <p className='text-light-secondary dark:text-dark-secondary'>
-                @twitter
+                @+
               </p>
               <div className='flex gap-1 text-light-secondary dark:text-dark-secondary'>
                 <i>·</i>
@@ -54,7 +54,7 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
               At the heart of Twitter are short messages called Tweets — just
               like this one — which can include photos, videos, links, text,
               hashtags, and mentions like{' '}
-              <span className='text-main-accent'>@twitter</span>.
+              <span className='text-main-accent'>@+</span>.
             </p>
           </div>
         </div>
