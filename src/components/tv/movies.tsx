73279d4832a5b11ce025e2fb302d84cc4947fd20
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-11 10:46:25 +1200

    Create movies.tsx

---
+++ src/components/tv/movies.tsx
@@ -0,0 +1,25 @@
+import { BlogData } from '../data/blogData';
+import Link from 'next/link';
+
+interface BlogProps {
+  blogData: BlogData[];
+}
+
+const Blog: React.FC<BlogProps> = ({ blogData }) => {
+  return (
+    <div className="space-y-4">
+      {blogData.map(blogData => (
+        <p key={blogData.post_id} className="flex items-center space-x-2">
+          <Link href={`/post/${blogData.post_id}`}>
+            <span className="text-xl font-bold">{blogData.post_id}</span>
+          </Link>
+          <Link href={`/post/${blogData.post_id}`}>
+            <b className="text-xl font-bold">{blogData.title}</b>
+          </Link>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
