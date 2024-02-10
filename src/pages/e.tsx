5cd4facd914d464da34699c10dff5c091a8c0b1b
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-10 12:26:14 +1200

    Update e.tsx

--- src/pages/e.tsx
+++ src/pages/e.tsx
@@ -1,18 +1,65 @@
+import { AnimatePresence } from 'framer-motion';
+import { where, orderBy } from 'firebase/firestore';
+import { useWindow } from '@lib/context/window-context';
+import { useInfiniteScroll } from '@lib/hooks/useInfiniteScroll';
+import { tweetsCollection } from '@lib/firebase/collections';
+import { HomeLayout, ProtectedLayout } from '@components/layout/common-layout';
+import { MainLayout } from '@components/layout/main-layout';
+import { SEO } from '@components/common/seo';
+import { MainContainer } from '@components/home/main-container';
+import { Input } from '@components/input/input';
+import { UpdateUsername } from '@components/home/update-username';
+import { MainHeader } from '@components/home/main-header';
+import { Tweet } from '@components/tweet/tweet';
+import { Loading } from '@components/ui/loading';
+import { Error } from '@components/ui/error';
+import type { ReactElement, ReactNode } from 'react';
 import Navbar from '@components/explore/explore-navbar';
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const { isMobile } = useWindow();
+
+  const { data, loading, LoadMore } = useInfiniteScroll(
+    tweetsCollection,
+    [where('parent', '==', null), orderBy('createdAt', 'desc')],
+    { includeUser: true, allowNull: true, preserve: true }
+  );
+
   return (
-    <div>
-      <Navbar
+    <MainContainer>
+      <SEO title='Explore | Aria+' />
+      <MainHeader
+        useMobileSidebar
+        title='Explore'
+        className='flex items-center justify-between'
+      >
+        <UpdateUsername />
+      </MainHeader>
+      {!isMobile && <Input />}
+     <Navbar
         sections={[
           { name: 'For You', href: '/' },
           { name: 'News', href: '/news' },
           { name: 'Entertainment', href: '/entertainment' },
         ]}
       />
-      <h1 className="text-4xl font-bold text-gray-800 mt-8">
-        Welcome to the Twitter Navbar!
-      </h1>
-    </div>
+            <AnimatePresence mode='popLayout'>
+              {data.map((tweet) => (
+                <Tweet {...tweet} key={tweet.id} />
+              ))}
+            </AnimatePresence>
+            <LoadMore />
+          </>
+        )}
+      </section>
+    </MainContainer>
   );
 }
+
+Home.getLayout = (page: ReactElement): ReactNode => (
+  <ProtectedLayout>
+    <MainLayout>
+      <HomeLayout>{page}</HomeLayout>
+    </MainLayout>
+  </ProtectedLayout>
+);
