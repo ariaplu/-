8e4e40afa6ec632554a71b1da15a236be2c0664b
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-10 12:37:55 +1200

    Update notifications.tsx

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -18,8 +18,8 @@ export default function Bookmarks(): JSX.Element {
 
   return (
     <MainContainer>
-      <SEO title='Trends | Aria+' />
-      <MainHeader useActionButton title='Trends' action={back}>
+      <SEO title='Notifications | Aria+' />
+      <MainHeader useActionButton title='Notifications' action={back}>
         <Button
           className='dark-bg-tab group relative ml-auto cursor-not-allowed p-2 hover:bg-light-primary/10
                      active:bg-light-primary/20 dark:hover:bg-dark-primary/10 dark:active:bg-dark-primary/20'
