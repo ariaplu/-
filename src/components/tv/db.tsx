40e94ed4723da3455fc809433cfb729aae3f5335
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-11 10:58:07 +1200

    Update and rename db.json to db.tsx

---
+++ src/components/tv/db.tsx
@@ -0,0 +1,15 @@
+interface BlogPost {
+  post_id: number;
+  title: string;
+  details: string;
+}
+
+const blogData: BlogPost[] = [
+  {
+    post_id: 1,
+    title: "You Are Not Just Talented, You Are Spiritually Gifted",
+    details:
+      "You Are Not Just Talented, You Are Spiritually Gifted Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  // ... other blog posts
+];
