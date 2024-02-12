e7229ebebb4f5b283b4088598f02edaef24dc2e9
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-13 01:12:22 +1200

    Update more-settings.tsx

--- src/components/sidebar/more-settings.tsx
+++ src/components/sidebar/more-settings.tsx
@@ -54,7 +54,7 @@ export function MoreSettings(): JSX.Element {
             <AnimatePresence>
               {open && (
                 <Menu.Items
-                  className='menu-container absolute -top-85 w-60 font-medium xl:w-11/12'
+                  className='menu-container absolute -top-80 w-60 font-medium xl:w-11/12'
                   as={motion.div}
                   {...variants}
                   static
