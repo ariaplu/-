a738d5afd38e4cff0404b8eb98f8dc2749903590
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-15 00:08:17 +1200

    Create exploreadfree.tsx

---
+++ src/components/explore/exploreadfree.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface Props {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const AdFreeComponent: React.FC<Props> = ({ onClick }) => {
+  return (
+    <div className="ad-free-container">
+      <h1 className="title">Go ad free</h1>
+      <p className="description">
+        Enjoy ad-free For You and Following timelines with Premium+
+      </p>
+      <button className="button" onClick={onClick}>
+        Subscribe
+      </button>
+    </div>
+  );
+};
+
+export default AdFreeComponent;
