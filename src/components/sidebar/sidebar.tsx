e7cc2e1bebd39b8ba5425cd191545973f4b32034
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-14 15:33:02 +1200

    Update sidebar.tsx

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -125,12 +125,12 @@ export function Sidebar(): JSX.Element {
             {!isMobile && <MoreSettings />}
           </nav>
           <Button
-            className='accent-tab justify-center object-center items-center block right-4 -translate-y-[72px] bg-main-accent text-lg font-bold text-white
+            className='accent-tab justify-center object-center items-center absolute right-4 -translate-y-[72px] bg-main-accent text-lg font-bold text-white
                        outline-none transition hover:brightness-90 active:brightness-75 xs:static xs:translate-y-0
                        xs:hover:bg-main-accent/90 xs:active:bg-main-accent/75 xl:w-11/12'
             onClick={openModal}
           > 
-        <ArplImage  className='block justify-self-center justify-center object-center items-center h-6 w-6 '
+        <ArplImage  className='absolute justify-self-center justify-center object-center items-center h-6 w-6 '
           imgClassName='arplicon'
           blurClassName='none'
           src='/assets/ariaplus.svg'
