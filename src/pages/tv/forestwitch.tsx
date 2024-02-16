31e4a05d0f2fcb2140e89ca3df45577951d483db
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-16 22:30:13 +1200

    Update forestwitch.tsx

--- src/pages/tv/forestwitch.tsx
+++ src/pages/tv/forestwitch.tsx
@@ -37,10 +37,10 @@ export default function Bookmarks(): JSX.Element {
             alt=''
             key=''
           />
-   <NextImage className='accent-tab relative -mt-40 pr-20 h-24 xs:h-24 sm:h-24 h-full w-24 rounded-none p-0 transition hover:brightness-75'
+   <NextImage className='accent-tab relative -mt-40 pl-20 h-24 xs:h-24 sm:h-24 h-full w-24 rounded-none p-0 transition hover:brightness-75'
             useSkeleton
             layout='fill'
-            imgClassName='object-cover'
+            imgClassName='object-contain'
             src='/assets/tv/movie/data/forestwitch/disneyplus.png'
             alt=''
             key=''
