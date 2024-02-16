8b62212ea0c93bd33de359919bdc66dc397fe85f
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-16 13:21:43 +1200

    Update movie.tsx

--- src/components/tv/movie.tsx
+++ src/components/tv/movie.tsx
@@ -1,191 +1,28 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { AnimatePresence, motion } from 'framer-motion';
-import cn from 'clsx';
-import { useAuth } from '@lib/context/auth-context';
-import { useModal } from '@lib/hooks/useModal';
-import { delayScroll } from '@lib/utils';
-import { Modal } from '@components/modal/modal';
-import { TweetReplyModal } from '@components/modal/tweet-reply-modal';
-import { ImagePreview } from '@components/input/image-preview';
-import { UserAvatar } from '@components/user/user-avatar';
-import { UserTooltip } from '@components/user/user-tooltip';
-import { UserName } from '@components/user/user-name';
-import { UserUsername } from '@components/user/user-username';
-import { TweetActions } from './tweet-actions';
-import { TweetStatus } from './tweet-status';
-import { TweetStats } from './tweet-stats';
-import { TweetDate } from './tweet-date';
-import type { Variants } from 'framer-motion';
-import type { Movie } from '@lib/types/movies';
-import type { User } from '@lib/types/user';
 
-export type MovieProps = Movie & {
-  user: User;
-  modal?: boolean;
-  pinned?: boolean;
-  profile?: User | null;
-  parentTweet?: boolean;
-};
-
-export const variants: Variants = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1, transition: { duration: 0.8 } },
-  exit: { opacity: 0, transition: { duration: 0.2 } }
-};
-
-export function Movie(movies: MovieProps): JSX.Element {
-  const {
-    id: moviesId,
-    text,
-    modal,
-    images,
-    parent,
-    pinned,
-    profile,
-    userLikes,
-    createdBy,
-    createdAt,
-    parentTweet,
-    userReplies,
-    userRetweets,
-    user: tweetUserData
-  } = movies;
-
-  const { id: ownerId, name, username, verified, photoURL } = tweetUserData;
-
-  const { user } = useAuth();
-
-  const { open, openModal, closeModal } = useModal();
-
-  const moviesLink = `/&/${moviesId}`;
-
-  const userId = user?.id as string;
-
-  const isOwner = userId === createdBy;
-
-  const { id: parentId, username: parentUsername = username } = parent ?? {};
-
-  const {
-    id: profileId,
-    name: profileName,
-    username: profileUsername
-  } = profile ?? {};
-
-  const reply = !!parent;
-  const tweetIsRetweeted = userRetweets.includes(profileId ?? '');
+const mComponent = () => {
+  const images = [
+    { src: '/assets/ariaplus.svg', href: '/link1' },
+    // Add more images as needed
+  ];
 
   return (
-    <motion.article
-      {...(!modal ? { ...variants, layout: 'position' } : {})}
-      animate={{
-        ...variants.animate,
-        ...(parentTweet && { transition: { duration: 0.2 } })
-      }}
-    >
-      <Modal
-        className='flex items-start justify-center'
-        modalClassName='bg-main-background rounded-2xl max-w-xl w-full my-8 overflow-hidden'
-        open={open}
-        closeModal={closeModal}
-      >
-
-      </Modal>
-      <Link href={moviesLink} scroll={!reply}>
-        <a
-          className={cn(
-            `accent-tab hover-card relative flex flex-col 
-             gap-y-4 px-4 py-3 outline-none duration-200`,
-            parentTweet
-              ? 'mt-0.5 pt-2.5 pb-0'
-              : 'border-b border-light-border dark:border-dark-border'
-          )}
-          onClick={delayScroll(200)}
-        >
-          <div className='grid grid-cols-[auto,1fr] gap-x-3 gap-y-1'>
-            <AnimatePresence initial={false}>
-              {modal ? null : pinned ? (
-                <TweetStatus type='pin'>
-                  <p className='text-sm font-bold'>Pinned Tweet</p>
-                </TweetStatus>
-              ) : (
-                tweetIsRetweeted && (
-                  <TweetStatus type='tweet'>
-                    <Link href={profileUsername as string}>
-                      <a className='custom-underline truncate text-sm font-bold'>
-                        {userId === profileId ? 'You' : profileName} Retweeted
-                      </a>
-                    </Link>
-                  </TweetStatus>
-                )
-              )}
-            </AnimatePresence>
-            <div className='flex flex-col items-center gap-2'>
-              
-            </div>
-            <div className='flex min-w-0 flex-col'>
-              <div className='flex justify-between gap-2 text-light-secondary dark:text-dark-secondary'>
-                <div className='flex gap-1 truncate xs:overflow-visible xs:whitespace-normal'>
-                  
-                    
-
-                </div>
-                <div className='px-4'>
-                  {!modal && (
-                    <TweetActions
-                      isOwner={isOwner}
-                      ownerId={ownerId}
-                      moviesId={moviesId}
-                      parentId={parentId}
-                      username={username}
-                      hasImages={!!images}
-                      createdBy={createdBy}
-                    />
-                  )}
-                </div>
-              </div>
-              {(reply || modal) && (
-                <p
-                  className={cn(
-                    'text-light-secondary dark:text-dark-secondary',
-                    modal && 'order-1 my-2'
-                  )}
-                >
-                  Replying to{' '}
-                  <Link href={`/user/${parentUsername}`}>
-                    <a className='custom-underline text-main-accent'>
-                      @{parentUsername}
-                    </a>
-                  </Link>
-                </p>
-              )}
-              {text && (
-                <p className='whitespace-pre-line break-words'>{text}</p>
-              )}
-              <div className='mt-1 flex flex-col gap-2'>
-                {images && (
-                  <ImagePreview
-                    tweet
-                    imagesPreview={images}
-                    previewCount={images.length}
-                  />
-                )}
-                {!modal && (
-                 <TweetStats
-                    reply={reply}
-                    userId={userId}
-                    isOwner={isOwner}
-                    moviesId={moviesId}
-                    userLikes={userLikes}
-                    userReplies={userReplies}
-                    userRetweets={userRetweets}
-                    openModal={!parent ? openModal : undefined}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-        </a>
-      </Link>
-    </motion.article>
+    <div className='grid grid-cols-3 gap-4'>
+      {images.map((image, index) => (
+        <Link key={index} href={image.href}>
+          <a className='relative h-40 overflow-hidden rounded-lg'>
+            <Image
+              src='/assets/ariaplus.svg'
+              layout='fill'
+              objectFit="cover'
+              alt={`Grid Image ${index + 1}`}
+            />
+          </a>
+        </Link>
+      ))}
+    </div>
   );
-}
+};
+
+export default mComponent;
