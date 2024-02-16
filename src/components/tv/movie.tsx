a161d46b1f58ae0ed9213f0e24787b1e8f6facaa
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-16 16:10:31 +1200

    Create movie.tsx

---
+++ src/components/tv/movie.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Movie = () => {
+  const images = [
+    { src: '/assets/ariaplus.svg', href: '/link1' },
+    // Add more images as needed
+  ];
+
+  return (
+    <div className='grid grid-cols-3 gap-4'>
+      {images.map((image, index) => (
+        <Link key={index} href={image.href}>
+          <a className='relative h-40 overflow-hidden rounded-lg'>
+            <Image
+              src={image.src}
+              layout='fill'
+              objectFit='cover'
+              alt={`Grid Image ${index + 1}`}
+            />
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Movie;
