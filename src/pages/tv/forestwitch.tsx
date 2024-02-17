13b9952a09f9dd7b69c2f38e1cd3a8ea60f9d6d9
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-17 15:37:35 +1200

    Update forestwitch.tsx

--- src/pages/tv/forestwitch.tsx
+++ src/pages/tv/forestwitch.tsx
@@ -42,7 +42,7 @@ export default function Bookmarks(): JSX.Element {
          </div>
          <div className='flex flex-col items-center p-8'>
 
-      <h1 className='text-3xl font-bold mt-8 text-center'>Forest Witch</h1>
+      <h1 className='text-3xl font-bold mt-5 text-center'>Forest Witch</h1>
       <div className='flex'>
        <Button className='dark-bg-tab group relative ml-auto p-2 hover:bg-light-primary/10
                      active:bg-light-primary/20 dark:hover:bg-dark-primary/10 dark:active:bg-dark-primary/20'>
