cc95262729dc75c07082adada3407b99eb27d77e
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-18 23:41:32 +1200

    Update movie-card.tsx

--- src/components/tv/movie-card.tsx
+++ src/components/tv/movie-card.tsx
@@ -18,20 +18,20 @@ const MovieCard = ({
   caution,
 }: MovieCardProps): ReactElement => {
   return (
-    <div className="bg-white shadow-md rounded-md p-8">
-      <h2 className="text-2xl font-bold mb-4">{title}</h2>
-      <p className="text-gray-700 mb-4">{description}</p>
-      <p className="text-gray-700 mb-4">
-        Directed by <span className="font-bold">{director}</span>
+    <div className='bg-white shadow-md rounded-md p-8'>
+      <h2 className='text-2xl font-bold mb-4'>{title}</h2>
+      <p className='text-gray-700 mb-4'>{description}</p>
+      <p className='text-gray-700 mb-4'>
+        Directed by <span className='font-bold'>{director}</span>
       </p>
-      <p className="text-gray-700 mb-4">
-        Age Rating: <span className="font-bold">{ageRating}</span>
+      <p className='text-gray-700 mb-4'>
+        Age Rating: <span className='font-bold'>{ageRating}</span>
       </p>
-      <p className="text-gray-700">
+      <p className='text-gray-700'>
         {caution}
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
