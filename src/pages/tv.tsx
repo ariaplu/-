2aa59e80bba079a78e4b98f0767ec9d95b015d3c
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-20 22:05:22 +1200

    Update tv.tsx

--- src/pages/tv.tsx
+++ src/pages/tv.tsx
@@ -19,7 +19,7 @@ import { MainContainer } from '@components/home/main-container';
 import { Modal } from '@components/modal/modal';
 import { ActionModal } from '@components/modal/action-modal';
 import { Movie } from '@components/tv/movie';
-import { TvBar } from '@components/tv/tv-bar';
+import { TvNav } from '@components/explore/tv-nav';
 import { StatsEmpty } from '@components/tweet/stats-empty';
 import { Button } from '@components/ui/button';
 import { ToolTip } from '@components/ui/tooltip';
@@ -94,7 +94,7 @@ export default function Bookmarks(): JSX.Element {
           />
         </Button>
       </MainHeader>
-      <TvBar/>
+      <TvNav/>
       <Movie/>
     </MainContainer>
   );
