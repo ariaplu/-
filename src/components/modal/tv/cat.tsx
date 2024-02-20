eee4af51807295bb996eda728a2b89a273b46118
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-20 23:56:24 +1200

    Update cat.tsx

--- src/components/modal/tv/cat.tsx
+++ src/components/modal/tv/cat.tsx
@@ -92,7 +92,7 @@ export function CatModal(): JSX.Element {
   return (
     <>
       <Modal
-        modalClassName='flex flex-col gap-6 max-w-xl bg-main-background w-full p-8 rounded-2xl h-[576px] bg-no-repeat bg-top bg-cover' style={{ backgroundImage: 'url(/assets/tv/movie/data/nowhere/nowhere-cover.jpg)' }}
+        modalClassName='flex flex-col gap-6 max-w-xl bg-main-background w-full p-8 rounded-2xl h-[576px] bg-no-repeat bg-top bg-cover' 
         open={open}
         closeModal={cancelUpdateUsername}
       >
