e538b5f480fac634085c3b6c6e5d7a4e18549f83
ariaplu <117322045+ariaplu@users.noreply.github.com>
2024-02-20 20:31:45 +1200

    Update wide-modal.tsx

--- src/components/modal/tv/wide-modal.tsx
+++ src/components/modal/tv/wide-modal.tsx
@@ -40,10 +40,8 @@ export function UsernameModal({
 
   return (
     <form
-      className='flex h-full flex-col justify-between'
-      onSubmit={changeUsername}
-    >
-    <div className='flex flex-col items-center gap-6 p-8 bg-no-repeat bg-top bg-cover' style={{ backgroundImage: 'url(/assets/tv/movie/data/nowhere/nowhere-cover.jpg)' }}>
+      className='flex h-full flex-col justify-between bg-no-repeat bg-top bg-cover' style={{ backgroundImage: 'url(/assets/tv/movie/data/nowhere/nowhere-cover.jpg)' }}>
+    <div className='flex flex-col items-center gap-6'>
         <div className='flex flex-col gap-4'>
           <i className='mx-auto'>
       <ArplImage
